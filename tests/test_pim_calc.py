4f4cfd47ab6b5aceb2e35f84113f9b4afc400d07
Jacek <jackkreft@gmail.com>
2018-05-09 21:08:32 +0200

    cleaned main and added fixes to 3rd calc

--- tests/test_pim_calc.py
+++ tests/test_pim_calc.py
@@ -7,6 +7,7 @@ import logging
 import pprint
 import numpy as np
 
+
 @pytest.fixture
 def pimc():
     return PIMCalc()
@@ -61,7 +62,7 @@ def test_calculate(pimc):
 
     tx_list = [1840.0, 1860.0]
     rx_list = [1820.0, 1900.0, 1910.0]
-    #pimc = PIMCalc()
+    
     pim_list = pimc.calculate(tx_list)
     print type(pim_list)
     assert isinstance(pim_list, tuple) is True
@@ -76,7 +77,6 @@ def test_calculate(pimc):
     test_im3 = np.array([1820.0, 1840.0, 1860.0, 1880.0])
     test_im5 = np.array([1800.0, 1820.0, 1840.0, 1860.0, 1880.0, 1900.0])
 
-    # logger.debug("compare: {0} vs {1}".format(test_im3, im3[0]))
     print "compare: {0} vs {1}".format(test_im3, im3[0])
     assert np.array_equal(test_im3, im3[0]) is True
     assert np.array_equiv(test_im3, im3[0]) is True
@@ -85,7 +85,6 @@ def test_calculate(pimc):
         print x, y
         assert x == y
 
-    # logger.debug("compare: {0} vs {1}".format(test_im5, im5[0]))
     print("compare: {0} vs {1}".format(test_im5, im5[0]))
     assert np.array_equal(test_im5, im5[0]) is True
     assert np.array_equiv(test_im5, im5[0]) is True
@@ -94,7 +93,6 @@ def test_calculate(pimc):
         print x, y
         assert x == y
     print("IM calc Seems ok..")
-    # logger.info("IM calc Seems ok..")
 
     im3_hits = pimc.check_rx(rx_list, im3[1])
     im5_hits = pimc.check_rx(rx_list, im5[1])
@@ -104,6 +102,38 @@ def test_calculate(pimc):
 
     print(im3_hits)
     print(im5_hits)
-    # logger.debug(im3_hits)
-    # logger.debug(im5_hits)
 
+def test_calculate_3rd_order(pimc):
+    tx_list = [1840.0, 1860.0]
+    tx_band = [10, 5.0]
+    rx_list = [1820.0, 1900.0, 1910.0]
+    
+    pim_list_5 = pimc.calculate(tx_list, tx_band, max_order=5)
+    assert len(pim_list_5) == 2
+    assert len(pim_list_5[1]) == 2
+    assert len(pim_list_5[1][0]) > 0
+    assert len(pim_list_5[1][1]) > 0
+    
+    pim_list_3 = pimc.calculate(tx_list, tx_band, max_order=3)
+    assert len(pim_list_3) == 2
+    assert len(pim_list_3[0]) == 2
+    assert len(pim_list_3[0][0]) > 0
+    assert len(pim_list_3[0][1]) > 0
+
+    # check that 5th order items are empty
+    assert len(pim_list_3[1]) == 2
+    assert len(pim_list_3[1][0]) == 0
+    assert len(pim_list_3[1][1]) == 0
+
+    # compare pim3 with pim3 from two calculations
+    for x in [0,1]:
+        assert np.array_equal(pim_list_3[0][x], pim_list_5[0][x]) is True
+        assert np.array_equiv(pim_list_3[0][x], pim_list_5[0][x]) is True
+        assert np.allclose(pim_list_3[0][x], pim_list_5[0][x]) is True
+        
+
+        for item_3, item_5 in zip(pim_list_3[0][x], pim_list_5[0][x]):
+            if x == 0:
+                assert item_3 == item_5
+            else:
+                assert np.allclose(item_3, item_5) is True
