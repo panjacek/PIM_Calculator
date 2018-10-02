3b76916f496cfab3f463011925ce2aaaa0837f50
Jacek Kreft <jacek.kreft@nokia.com>
2018-10-02 12:53:29 +0200

    Typo fix in import

--- PIM_Calculator/pimQt.py
+++ PIM_Calculator/pimQt.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python
-from __future__ import print_fuction
+from __future__ import print_function
 from __future__ import unicode_literals
 import sys
 # from PySide2.QtWidgets import QApplication, QWidget, QLabel, QPushButton
